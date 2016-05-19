16f41a26d411ae90609506a75bdf0bdb8562e4da
achernyak <artem@ikayzo.com>
2016-05-19 10:14:16 -0500

    html zipper

---
+++ html_zipper.clj
@@ -0,0 +1,11 @@
+(require '[clojure.zip :as z])
+
+(defn html-zip [root]
+  (z/zipper
+   vector?
+   (fn [[tagname & xs]]
+     (if (map? (first xs)) (next xs) xs))
+   (fn [[tagname & xs] children]
+     (into (if (map? (first xs)) [tagname (first xs)] [tagname])
+           children))
+   root))
