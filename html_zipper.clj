a0aba1dab674f8c76c75553d88103a27e8bd8567
achernyak <artem@ikayzo.com>
2016-05-19 10:21:00 -0500

    wrapping elements

--- html_zipper.clj
+++ html_zipper.clj
@@ -9,3 +9,15 @@
      (into (if (map? (first xs)) [tagname (first xs)] [tagname])
            children))
    root))
+
+(defn wrap
+  "Wraps the current node in the specified tag and attributes."
+  ([loc tag]
+   (z/edit loc #(vector tag %)))
+  ([loc tag attrs]
+   (z/edit loc #(vector tag attrs %))))
+
+(def h [:body [:h1 "Clojure"]
+        [:p "What a wonderful language!"]])
+
+(-> h html-zip z/down z/right z/down (wrap :b) z/root)
