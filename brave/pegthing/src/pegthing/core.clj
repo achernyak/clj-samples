0d6b9fa324b126e44eedbd9c31697d9997a22c02
achernyak <artem@ikayzo.com>
2016-05-21 15:50:44 -0500

    inti reqs

--- brave/pegthing/src/pegthing/core.clj
+++ brave/pegthing/src/pegthing/core.clj
@@ -1,5 +1,8 @@
 (ns pegthing.core
+  (require [clojure.set :as set])
   (:gen-class))
+
+(declare successful-move prompt-move game-over query-rows)
 
 (defn -main
   "I don't do a whole lot ... yet."
