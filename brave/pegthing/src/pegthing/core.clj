00e61ecb9d868c086fb8880062bdd5ef1364d2d2
achernyak <artem@ikayzo.com>
2016-05-21 19:26:01 -0500

    board functions

--- brave/pegthing/src/pegthing/core.clj
+++ brave/pegthing/src/pegthing/core.clj
@@ -60,3 +60,21 @@
         neighbor (+ 1 row pos)
         destination (+ 2 row neighbor)]
     (connect board max-pos pos neighbor destination)))
+
+(defn add-pos
+  "Pegs teh position and performs connections"
+  [board max-pos pos]
+  (let [pegged-board (assoc-in board [pos :pegged] true)]
+    (reduce (fn [new-board connection-creation-fn]
+              (connection-creation-fn new-board max-pos pos))
+            pegged-board
+            [connect-right connect-down-left connect-down-right])))
+
+(defn new-board
+  "Creates a new board with the given number of rows"
+  [rows]
+  (let [initial-board {:rows rows}
+        max-pos (row-tri rows)]
+    (reduce (fn [board pos] (add-pos board max-pos pos))
+            initial-board
+            (range 1 (inc max-pos)))))
