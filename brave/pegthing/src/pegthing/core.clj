90b736084845747e8e1bb2ef16eb4adc52f44230
achernyak <artem@ikayzo.com>
2016-05-21 20:02:36 -0500

    printing board

--- brave/pegthing/src/pegthing/core.clj
+++ brave/pegthing/src/pegthing/core.clj
@@ -157,3 +157,27 @@
          (colorize "0" :blue)
          (colorize "-" :red))))
 
+(defn row-positions
+  "Return all positions in the given row"
+  [row-num]
+  (range (inc (or (row-tri (dec row-num)) 0))
+         (inc (row-tri row-num))))
+
+(defn row-padding
+  "String of spaces to add to the beginning of a row to center it"
+  [row-num rows]
+  (let [pad-length (/ (* (- rows row-num) pos-chars)2)]
+    (apply str (take pad-length (repeat " ")))))
+
+(defn render-row
+  [board row-num]
+  (str (row-padding row-num (:rows board))
+       (clojure.string/join " " (map (partial render-pos board)
+                                     (row-positions row-num)))))
+
+(defn print-board
+  [board]
+  (doseq [row-num (range 1 (inc (:rows board)))]
+    (println (render-row board row-num))))
+
+
