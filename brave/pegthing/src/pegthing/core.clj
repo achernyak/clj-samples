fda145ab10e0d8f86f9046219a882df57ef44c65
achernyak <artem@ikayzo.com>
2016-05-21 19:03:53 -0500

    row num functions

--- brave/pegthing/src/pegthing/core.clj
+++ brave/pegthing/src/pegthing/core.clj
@@ -4,7 +4,28 @@
 
 (declare successful-move prompt-move game-over query-rows)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+   (let [new-sum (+ sum n)]
+     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(def tri (tri*))
+
+(defn triangular?
+  "Is the number triangular? e.g. 1, 3, 6, 10, 15, etc"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(defn row-tri
+  "The triangular number at the end of row n"
+  [n]
+  (last (take n tri)))
+
+(defn row-num
+  "Returns row number the position belogs to: pos 1 in row 1,
+  positions 2 and 3 in row 2, etc."
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
