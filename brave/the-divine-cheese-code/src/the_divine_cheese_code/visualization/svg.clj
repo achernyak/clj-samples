f661342cd1cea8333e3e68e51ad62590a9cfda73
achernyak <artem@ikayzo.com>
2016-05-22 18:32:48 -0500

    heist points

--- brave/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
+++ brave/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
@@ -1,4 +1,16 @@
-(ns the-divine-cheese-code.visualization.svg)
+(ns the-divine-cheese-code.visualization.svg
+  (:require [clojure.string :as s])
+  (:refer-clojure :exclude [min max]))
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+            (map (fn [k] (apply comparison-fn (map k maps)))
+                 ks))))
+
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
 
 (defn latlng->point
   "convert lat/lng map to comma-separated string"
