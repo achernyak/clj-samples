afafc6ecf81f5e1a616527efccfc5bdb5c2235b0
achernyak <artem@ikayzo.com>
2016-05-22 20:29:12 -0500

    infix 2

--- brave/read_eval.clj
+++ brave/read_eval.clj
@@ -6,3 +6,7 @@
   "Use this macro when you pine for a notation of your childhood"
   [infixed]
   (list (second infixed) (first infixed) (last infixed)))
+
+(defmacro infix-2
+  [[operand1 op operand2]]
+  (list op operand1 operand2))
