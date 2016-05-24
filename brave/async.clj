e9960aa95ca7d5cee82c20b07e2e0ea07728b31c
achernyak <artem@ikayzo.com>
2016-05-24 08:15:47 -0500

    yak butter

--- brave/async.clj
+++ brave/async.clj
@@ -13,3 +13,27 @@
       (doseq [headshot gimli-headshots]
         (future (upload-document headshot)
                 (force notify))))
+
+(def yak-butter-internation
+  {:store "Yak Butter International"
+   :price 90
+   :smoothness 90})
+(def butter-than-nothing
+  {:store "Butter Than Nothing"
+   :price 150
+   :smoothness 83})
+(def baby-got-yak
+  {:store "Baby Got Yak"
+   :price 94
+   :smoothness 99})
+
+(defn mock-api-call
+  [result]
+  (Thread/sleep 1000)
+  result)
+
+(defn satisfactory?
+  [butter]
+  (and (<= (:price butter) 100)
+       (>= (:smoothness butter) 97)
+       butter))
