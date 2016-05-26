100b303fb456bf8c737662e829eb05ad0bbf728a
achernyak <artem@ikayzo.com>
2016-05-26 13:39:36 -0500

    var binding

---
+++ brave/more-async/vars.clj
@@ -0,0 +1,7 @@
+(def ^:dynamic *notification-address* "dobby@elf.org")
+
+(binding [*notification-address* "tester-1@elf.org"]
+  (println *notification-address*)
+  (binding [*notification-address* "tester-2@elf.org"]
+    (println *notification-address*))
+  (println *notification-address*))
