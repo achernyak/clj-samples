86eecfe50670389dc021fc5933ab354b30be95fc
achernyak <artem@ikayzo.com>
2016-05-26 11:34:52 -0500

    sock and gnome generator

---
+++ brave/more-async/socks.clj
@@ -0,0 +1,20 @@
+(def sock-varieties
+  #{"darned" "argyle" "wool" "horsehair" "mulleted"
+    "passive-aggressive" "striped" "polka-dotted"
+    "athletic" "business" "power" "invisible" "gollumed"})
+
+(defn sock-count
+  [sock-variety count]
+  {:variety sock-variety
+   :count count})
+
+(defn generate-sock-gnome
+  "Create an inital sock gnome state with no socks"
+  [name]
+  {:name name
+   :socks #{}})
+
+(def sock-gnome (ref (generate-sock-gnome "Barumpharumph")))
+
+(def dryer (ref {:name "LG 1337"
+                 :sock (set (map #(sock-count % 2) sock-varieties))}))
