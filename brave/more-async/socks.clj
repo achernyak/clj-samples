d0dcc20a5c05eea669db54f394b2ac427cb5ff9b
achernyak <artem@ikayzo.com>
2016-05-26 12:40:18 -0500

    commute

--- brave/more-async/socks.clj
+++ brave/more-async/socks.clj
@@ -32,4 +32,27 @@
   [target-sock sock-set]
   (filter #(= (:variety %) (:variety target-sock)) sock-set))
 
+(defn sleep-print-update
+  [sleep-time thread-name update-fn]
+  (fn [state]
+    (Thread/sleep sleep-time)
+    (println (str thread-name ": " state))
+    (update-fn state)))
+
+(def counter (ref 0))
+(future (dosync (commute counter (sleep-print-update 100 "Thread A" inc))))
+(future (dosync (commute counter (sleep-print-update 150 "Thread B" inc))))
+
+(def receiver-a (ref #{}))
+(def receiver-b (ref #{}))
+(def giver (ref #{1}))
+(do (future (dosync (let [gift (first @giver)]
+                      (Thread/sleep 10)
+                      (commute receiver-a conj gift)
+                      (commute giver disj gift))))
+    (future (dosync (let [gift (first @giver)]
+                      (Thread/sleep 50)
+                      (commute receiver-b conj gift)
+                      (commute giver disj gift)))))
+
 
