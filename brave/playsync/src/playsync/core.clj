8886628484fa9c30e972732d69af7a253f91530e
achernyak <artem@ikayzo.com>
2016-05-27 22:12:20 -0500

    quote grabber

--- brave/playsync/src/playsync/core.clj
+++ brave/playsync/src/playsync/core.clj
@@ -45,3 +45,24 @@
   (upload "sassy.jpg" c3)
   (let [[headshot channel] (alts!! [c1 c2 c3])]
     (println "Sending headshot notification for" headshot)))
+
+(defn append-to-file
+  "Write a string to the end of a file"
+  [filename s]
+  (spit filename s :append true))
+
+(defn format-quote
+  "Delineate the beginning and end of a quote because it's convenient"
+  [quote]
+  (str "=== BEGIN QUOTE ===\n" quote "=== END QUOTE ===\n\n"))
+
+(defn random-quote
+  "Retrieve a random quote and format it"
+  []
+  (format-quote (slurp "http://www.braveclojure.com/random-quote")))
+
+(defn snag-quote
+  [filename num-quotes]
+  (let [c (chan)]
+    (go (while true (append-to-file filename (<! c))))
+    (dotimes [n num-quotes] (go (>! c (random-quote))))))
