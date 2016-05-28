be33fb3b5078bd88fa56b09becb13da2e231441b
achernyak <artem@ikayzo.com>
2016-05-27 21:17:23 -0500

    hot dog machine

--- brave/playsync/src/playsync/core.clj
+++ brave/playsync/src/playsync/core.clj
@@ -11,3 +11,24 @@
 (def hi-chan (chan))
 (doseq [n (range 1000)]
   (go (>! hi-chan (str "hi " n))))
+
+(defn hot-dog-machine
+  [hot-dog-count]
+  (let [in (chan)
+        out (chan)]
+    (go (loop [hc hot-dog-count]
+          (if (> hc 0)
+            (let [input (<! in)]
+              (if (= 3 input)
+                (do (>! out "hot dog")
+                    (recur (dec hc)))
+                (do (>! out "wilted lettuce")
+                    (recur hc))))
+            (do (close! in)
+                (close! out)))))
+    [in out]))
+
+(let [[in out] (hot-dog-machine 5)]
+  (>!! in "pocket lin")
+  (<!! out))
+
