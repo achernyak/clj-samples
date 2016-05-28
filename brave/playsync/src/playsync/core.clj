4fc6e3288f0a9d4e5ee656519ae16060b048f15f
achernyak <artem@ikayzo.com>
2016-05-27 21:44:44 -0500

    async upload

--- brave/playsync/src/playsync/core.clj
+++ brave/playsync/src/playsync/core.clj
@@ -32,3 +32,16 @@
   (>!! in "pocket lin")
   (<!! out))
 
+(defn upload
+  [headhsot c]
+  (go (Thread/sleep (rand 100))
+      (>! c headhsot)))
+
+(let [c1 (chan)
+      c2 (chan)
+      c3 (chan)]
+  (upload "serious.jpg" c1)
+  (upload "fun.jpg" c2)
+  (upload "sassy.jpg" c3)
+  (let [[headshot channel] (alts!! [c1 c2 c3])]
+    (println "Sending headshot notification for" headshot)))
